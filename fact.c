3af4e2a1e3f899cf054832b22ee1f4ec73a2f75c
ManishaDevaraj <manisha23.MD@gmail.com>
2016-10-20 15:34:35 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num,fact=1,i;
+printf("Enter the number ");
+scanf("%d", num);
+if(num=0)
+{
+return 1;
+}
+else
+{
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+}
+printf("The factorial is  %d", fact);
+}
