2fa99d33c5de94e1947d593762c8c1542c43fc2b
ManishaDevaraj <manisha23.MD@gmail.com>
2016-10-20 15:11:07 +0530

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char a[10],b[10];
+printf("Enter the first string ");
+gets(a);
+printf("Enter the second string ");
+gets(b);
+if(a==b)
+printf("Equal");
+else 
+printf("Not Equal");
+}
