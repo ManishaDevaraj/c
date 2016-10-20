8ca45363aecfd4907bbb4de5935d719a0749eb39
ManishaDevaraj <manisha23.MD@gmail.com>
2016-10-20 15:17:17 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char a[10],b[10];
+printf("Enter the first string ");
+gets(a);
+printf("Enter the second string");
+gets(b);
+printf("The String Concadination is %s",a&b);
+}
