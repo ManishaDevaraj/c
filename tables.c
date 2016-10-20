5c7181093e3c1b5a24c9fb4d64c6641ab94d176d
ManishaDevaraj <manisha23.MD@gmail.com>
2016-10-20 15:26:34 +0530

    Create tables.c

---
+++ tables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n,i,mul=0;
+printf("Enter which table should be performed");
+scanf("%d",&n);
+for(i=1;i<=10;i++)
+{
+mul=n*i;
+printf("The %d tables is %d",n,sum);
+}
+}
